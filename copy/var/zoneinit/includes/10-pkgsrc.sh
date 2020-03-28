9009083dc00b0e7b1442d55f1917ad59cc754bf5
lichunxiao <lichunxiao@wiz.top>
2020-03-28 22:06:46 +0800

    debug

--- copy/var/zoneinit/includes/10-pkgsrc.sh
+++ copy/var/zoneinit/includes/10-pkgsrc.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-if /usr/sbin/mdata-get pkgsrc 1>/dev/null 2>&1; then
+if [[ /usr/sbin/mdata-get pkgsrc 1>/dev/null 2>&1 ]]; then
     PKGSRC=$(/usr/sbin/mdata-get pkgsrc)
     BRAND=$(/usr/sbin/mdata-get sdc:brand)
-    if [ "${BRAND}" == "joyent-minimal" ];then
+    if [[ "${BRAND}" == "joyent-minimal" ]];then
         sed -i '' "s|^http.*|${PKGSRC}|" /opt/local/etc/pkgin/repositories.conf;
 		sed -i '' "s|^PKG_PATH=.*|PKG_PATH=${PKGSRC}|" /opt/local/etc/pkg_install.conf
     else
