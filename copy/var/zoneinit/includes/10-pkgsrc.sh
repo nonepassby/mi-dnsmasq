002daba5808b59cfcd50767ac7db7a905641bd0f
lichunxiao <lichunxiao@wiz.top>
2020-03-28 23:20:26 +0800

    sed

--- copy/var/zoneinit/includes/10-pkgsrc.sh
+++ copy/var/zoneinit/includes/10-pkgsrc.sh
@@ -2,12 +2,10 @@
 
 if /usr/sbin/mdata-get pkgsrc 1>/dev/null 2>&1; then
     PKGSRC=$(/usr/sbin/mdata-get pkgsrc)
-    BRAND=$(/usr/sbin/mdata-get sdc:brand)
-    if [[ "${BRAND}" == "joyent-minimal" ]];then
-        sed -i '' "s|^http.*|${PKGSRC}|" /opt/local/etc/pkgin/repositories.conf;
-		sed -i '' "s|^PKG_PATH=.*|PKG_PATH=${PKGSRC}|" /opt/local/etc/pkg_install.conf
+    if sed -i "s|^http.*|${PKGSRC}|" /opt/local/etc/pkgin/repositories.conf 1>/dev/null 2>&1; then
+        sed -i "s|^PKG_PATH=.*|PKG_PATH=${PKGSRC}|" /opt/local/etc/pkg_install.conf;
     else
-        sed -i "s|^http.*|${PKGSRC}|" /opt/local/etc/pkgin/repositories.conf;
-		sed -i "s|^PKG_PATH=.*|PKG_PATH=${PKGSRC}|" /opt/local/etc/pkg_install.conf 
+        sed -i '' "s|^http.*|${PKGSRC}|" /opt/local/etc/pkgin/repositories.conf;
+        sed -i '' "s|^PKG_PATH=.*|PKG_PATH=${PKGSRC}|" /opt/local/etc/pkg_install.conf
     fi
 fi
